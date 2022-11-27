62f00d353ef845a6adb8ffe0c7049131e21f139a
alinavarkki <alina@viaeurope.com>
2022-11-27 21:35:57 +0000

    added startAdornment for the search field. Change the placeholder to a real logo.

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
-import { Grid, TextField, Typography } from '@mui/material'
+import { Grid, TextField, Typography, Autocomplete, InputAdornment } from '@mui/material'
 import './_SearchPage.scss'
+import { postcodes } from '../../resources/postcodeExamples'
 
 function SearchPage (): ReactElement {
   return (
@@ -10,11 +11,10 @@ function SearchPage (): ReactElement {
                 direction="column"
                 alignItems="center"
                 height="100vh"
+                spacing={2}
             >
                 <Grid item xs={3}>
-                    <p className="grid">
-                        Logo
-                    </p>
+                    <img className="imageIcon" src="https://www.energymutual.com/wp-content/uploads/2020/03/EM-1-cropped.svg"/>
                 </Grid>
                 <Grid item xs={3}>
                     <Typography variant="h4" className="gridText" align="center" sx={{ fontWeight: 600 }}>
@@ -25,9 +25,21 @@ function SearchPage (): ReactElement {
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
-                    <TextField
-                        label="Search location"
-                        variant="filled"
+                    <Autocomplete
+                        disablePortal
+                        className="postcodeSearch"
+                        id="combo-box-demo"
+                        options={postcodes}
+                        sx={{ width: 300 }}
+                        renderInput={(params) =>
+                            <TextField
+                                {...params}
+                                label="Postcode"
+                                InputProps={{
+                                  ...params.InputProps,
+                                  startAdornment: <InputAdornment position="start">kg</InputAdornment>
+                                }}
+                            />}
                     />
                 </Grid>
             </Grid>
---
+++ src/resources/postcodeExamples.ts
@@ -0,0 +1,4 @@
+export const postcodes = [
+  'SW1W 0NY', 'PO16 7GZ', 'GU16 7HF', 'L1 8JQ',
+  'G1 1AB', 'G2 6JI', 'GU16 7HF', 'G40 8UH'
+]
