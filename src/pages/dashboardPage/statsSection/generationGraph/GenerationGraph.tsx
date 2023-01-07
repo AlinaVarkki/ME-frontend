9a35e90922bf91fc5470e44f3d1819049f3ae9e6
alinavarkki <alina@viaeurope.com>
2023-01-07 08:19:16 +0200

    Styled the graph. Added custom line and dot. Added a box around the graph.

--- src/pages/dashboardPage/statsSection/generationGraph/GenerationGraph.tsx
+++ src/pages/dashboardPage/statsSection/generationGraph/GenerationGraph.tsx
@@ -1,29 +1,41 @@
+import './_GenerationGraph.scss'
+
 import React, { ReactElement } from 'react'
 import {
   AreaChart, Area, XAxis,
   YAxis,
-  Tooltip
+  Tooltip, ResponsiveContainer
 } from 'recharts'
 import { genGraphMockData } from '../../../../resources/genGraphMockData'
 
 function GenerationGraph (): ReactElement {
   return (
-      <AreaChart
-          width={500}
-          height={400}
-          data={genGraphMockData}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0
-          }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+      <div className="graphWrapper">
+      <ResponsiveContainer width="100%" height={340}>
+          <AreaChart
+              className="graph"
+              data={genGraphMockData}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0
+              }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip cursor={{ stroke: '#121733', strokeWidth: 2 }} />
+            <Area
+                type="monotone"
+                dataKey="uv"
+                stroke='#33ddac'
+                strokeWidth={3}
+                fill='#adf1de'
+                activeDot={{ stroke: '#121733', strokeWidth: 4, fill: '#33ddac', r: 7 }}
+            />
+          </AreaChart>
+      </ResponsiveContainer>
+      </div>
   )
 }
 
