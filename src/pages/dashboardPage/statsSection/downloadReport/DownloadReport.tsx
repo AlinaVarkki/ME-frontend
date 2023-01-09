04c26bed8d81aaeb6bc981cb3dbd77f533562650
alinavarkki <alina@viaeurope.com>
2023-01-09 11:26:56 +0200

    Component to download report ready.

--- src/pages/dashboardPage/statsSection/downloadReport/DownloadReport.tsx
+++ src/pages/dashboardPage/statsSection/downloadReport/DownloadReport.tsx
@@ -1,11 +1,25 @@
 import './_DownloadReport.scss'
 
 import React, { ReactElement } from 'react'
+import { Button, Typography } from '@mui/material'
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 
 function DownloadReport (): ReactElement {
   return (
-      <div>
-        download report
+      <div className="downloadReportContainer">
+          <Typography className="downloadReportElement" fontSize={20} fontWeight="bold">
+              Reach your energy goals faster
+          </Typography>
+          <Typography className="downloadReportElement">
+            Use Energy to interpret and compare your electricity production to reach best performance
+          </Typography>
+          <Button
+              variant="contained"
+              startIcon={<FileDownloadOutlinedIcon/>}
+              className="downloadButton downloadReportElement"
+              style={{ backgroundColor: '#121733', marginTop: '10px', borderRadius: '30px' }}>
+              Download Report
+          </Button>
       </div>
   )
 }
