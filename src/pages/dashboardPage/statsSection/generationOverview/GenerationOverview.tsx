337d6f9268b1f99648c884b02300258debe9dd14
alinavarkki <alina@viaeurope.com>
2023-01-09 08:17:39 +0200

    Icon now is chosen through enum and added padding on mobile for generation overview boxes.

--- src/pages/dashboardPage/statsSection/generationOverview/GenerationOverview.tsx
+++ src/pages/dashboardPage/statsSection/generationOverview/GenerationOverview.tsx
@@ -2,14 +2,48 @@ import './_GenerationOverview.scss'
 
 import React, { ReactElement } from 'react'
 import GenerationOverviewBox from './generationOverviewBox/GenerationOverviewBox'
-import { generationOverviewInfo } from '../../../../resources/GenerationOverviewMockInfo'
+
+export enum GenerationBoxColorsEnum {
+  green = 'green',
+  blue = 'blue'
+}
+
+export enum GenerationBoxIconsEnum {
+  dashCircle = 'dashCircle',
+  partCircle = 'partCircle',
+  fullCircle = 'fullCircle'
+}
+
+const generationOverviewInfo = [
+  {
+    valueDescription: 'Total Generation',
+    icon: GenerationBoxIconsEnum.dashCircle,
+    value: '200MWh'
+  },
+  {
+    valueDescription: 'Current Month Generation',
+    icon: GenerationBoxIconsEnum.partCircle,
+    value: '1MWh'
+  },
+  {
+    valueDescription: 'Average Load Factor',
+    icon: GenerationBoxIconsEnum.fullCircle,
+    value: '84%'
+  }
+]
 
 function GenerationOverview (): ReactElement {
   return (
       <div className="GenerationOverviewContainer">
           {
               generationOverviewInfo.map((genOverviewItem, index) => (
-                  <GenerationOverviewBox key={index} value={genOverviewItem.value} valueDescription={genOverviewItem.valueDescription} backgroundColour={index === generationOverviewInfo.length - 1 ? 'green' : 'blue'}/>
+                  <GenerationOverviewBox
+                      key={index}
+                      value={genOverviewItem.value}
+                      valueDescription={genOverviewItem.valueDescription}
+                      backgroundColour={index === generationOverviewInfo.length - 1 ? GenerationBoxColorsEnum.green : GenerationBoxColorsEnum.blue}
+                      icon={genOverviewItem.icon}
+                  />
               ))
           }
       </div>
--- src/pages/dashboardPage/statsSection/generationOverview/generationOverviewBox/GenerationOverviewBox.tsx
+++ src/pages/dashboardPage/statsSection/generationOverview/generationOverviewBox/GenerationOverviewBox.tsx
@@ -3,20 +3,35 @@ import './_GenerationOverviewBox.scss'
 import React, { ReactElement } from 'react'
 import { Typography } from '@mui/material'
 
-import Brightness1OutlinedIcon from '@mui/icons-material/Brightness1Outlined'
-
-type GenerationBoxColors = 'green' | 'blue'
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined'
+import DataUsageOutlinedIcon from '@mui/icons-material/DataUsageOutlined';
+import { GenerationBoxColorsEnum, GenerationBoxIconsEnum } from '../GenerationOverview'
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
 
 interface GenerationOverviewInfoBoxI {
   valueDescription: string
   value: string
-  backgroundColour: GenerationBoxColors
+  backgroundColour: GenerationBoxColorsEnum
+  icon: GenerationBoxIconsEnum
 }
 
-function GenerationOverviewBox ({ valueDescription, value, backgroundColour }: GenerationOverviewInfoBoxI): ReactElement {
+function GenerationOverviewBox ({ valueDescription, value, backgroundColour, icon }: GenerationOverviewInfoBoxI): ReactElement {
+  const getIcon = (icon: GenerationBoxIconsEnum): ReactElement => {
+    switch (icon) {
+      case GenerationBoxIconsEnum.dashCircle:
+        return <PowerSettingsNewOutlinedIcon className="valueIcon" fontSize="large"/>
+      case GenerationBoxIconsEnum.partCircle:
+        return <DataUsageOutlinedIcon className="valueIcon" fontSize="large"/>
+      case GenerationBoxIconsEnum.fullCircle:
+        return <CircleOutlinedIcon className="valueIcon" fontSize="large"/>
+      default:
+        return <CircleOutlinedIcon className="valueIcon" fontSize="large"/>
+    }
+  }
+
   return (
-      <div className={backgroundColour === 'green' ? 'genOverviewBoxContainer green' : 'genOverviewBoxContainer'}>
-          <Brightness1OutlinedIcon className="valueIcon" fontSize="large"/>
+      <div className={backgroundColour === GenerationBoxColorsEnum.green ? 'genOverviewBoxContainer green' : 'genOverviewBoxContainer'}>
+          {getIcon(icon)}
           <Typography className="valueDescription">
               {valueDescription}
           </Typography>
